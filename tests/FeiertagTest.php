7802145f1ecece6127b28d0a257b143b1d34b3e3
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2016-08-19 23:52:06 +0200

    covered remaining methods of Feiertag

--- tests/FeiertagTest.php
+++ tests/FeiertagTest.php
@@ -7,6 +7,36 @@ use intrawarez\feiertage\Feiertag;
 
 class FeiertagTest extends TestCase {
 	
+	public function testKeys () {
+		
+		$keys = Feiertag::keys();
+		
+		$this->assertInternalType("array", $keys);
+		
+		$this->assertEquals(19, count($keys));
+		
+		$this->assertContains(Feiertag::NEUJAHRSTAG, $keys);
+		$this->assertContains(Feiertag::HEILIGEDREIKOENIGE, $keys);
+		$this->assertContains(Feiertag::GRUENDONNERSTAG, $keys);
+		$this->assertContains(Feiertag::KARFREITAG, $keys);
+		$this->assertContains(Feiertag::OSTERSONNTAG, $keys);
+		$this->assertContains(Feiertag::OSTERMONTAG, $keys);
+		$this->assertContains(Feiertag::TAGDERARBEIT, $keys);
+		$this->assertContains(Feiertag::CHRISTIHIMMELFAHRT, $keys);
+		$this->assertContains(Feiertag::PFINGSTSONNTAG, $keys);
+		$this->assertContains(Feiertag::PFINGSTMONTAG, $keys);
+		$this->assertContains(Feiertag::FRONLEICHNAM, $keys);
+		$this->assertContains(Feiertag::AUGSBURGERFRIEDENSFEST, $keys);
+		$this->assertContains(Feiertag::MARIAEHIMMELFAHRT, $keys);
+		$this->assertContains(Feiertag::TAGDERDEUTSCHENEINHEIT, $keys);
+		$this->assertContains(Feiertag::REFORMATIONSTAG, $keys);
+		$this->assertContains(Feiertag::ALLERHEILIGEN, $keys);
+		$this->assertContains(Feiertag::BUSSUNDBETTAG, $keys);
+		$this->assertContains(Feiertag::ERSTERWEIHNACHTSTAG, $keys);
+		$this->assertContains(Feiertag::ZWEITERWEIHNACHTSTAG, $keys);
+		
+	}
+	
 	public function test2016 () {
 	
 		$jahr = 2016;
@@ -31,6 +61,34 @@ class FeiertagTest extends TestCase {
 		$this->assertEquals(Feiertag::ErsterWeihnachtstag($jahr)->getDate(), 	date_create("2016-12-25"));
 		$this->assertEquals(Feiertag::ZweiterWeihnachtstag($jahr)->getDate(),	date_create("2016-12-26"));
 		
+	}
+	
+	public function testDateTimeInterface () {
+		
+		$feiertag = Feiertag::Neujahrstag(2016);
+		$date = $feiertag->getDate();
+		
+		$this->assertEquals($feiertag->format(\DateTime::ISO8601), $date->format(\DateTime::ISO8601));
+		$this->assertEquals($feiertag->getOffset(), $date->getOffset());
+		$this->assertEquals($feiertag->getTimestamp(), $date->getTimestamp());
+		$this->assertEquals($feiertag->getTimezone(), $date->getTimezone());
+		
+	}
+	
+	public function testToDateTime () {
+		
+		$date = Feiertag::Neujahrstag(2016)->toDateTime();
+		
+		$this->assertInstanceOf(\DateTime::class, $date);
+		
+	}
+	
+	public function testToDateTimeImmutable () {
+	
+		$date = Feiertag::Neujahrstag(2016)->toDateTimeImmutable();
+	
+		$this->assertInstanceOf(\DateTimeImmutable::class, $date);
+	
 	}
 	
 	
