1a8a0b9ffcaed9302822d1e89c74a40e6754aba7
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2020-03-14 16:20:37 +0100

    removed docs: this project should be easy/small enough to understand without generated docs

--- docs.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -f sami.phar ]; then
-	echo "\nDownloading: http://get.sensiolabs.org/sami.phar\n" ;
-	wget http://get.sensiolabs.org/sami.phar ;
-fi
-php sami.phar update samicfg.php ;
-echo "\nRemoving: cache\n" ;
-rm -r cache ;
